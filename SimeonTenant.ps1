4590d79a36a69eb2d7dcadec7d08a6a7311e4034
admin-simeon <->
2020-10-05 13:11:17 -0700

    add tenant id

--- SimeonTenant.ps1
+++ SimeonTenant.ps1
@@ -285,7 +285,7 @@ CRLFOption=CRLFAlways
 
         if ($AzureManagementAccessToken -and $AzureADGraphAccessToken) {
             Connect-AzAccount -AccessToken $AzureManagementAccessToken -AccountId $TenantId
-            Connect-AzureAD -AadAccessToken $AzureADGraphAccessToken -AccountId $TenantId
+            Connect-AzureAD -AadAccessToken $AzureADGraphAccessToken -AccountId $TenantId -TenantId $TenantId
             return
         }
 
