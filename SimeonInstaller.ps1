998c29cfbc713b4e8dd3eb2228d001393c6d7629
lance-Simeon <lance@simeoncloud.com>
2021-03-24 20:19:46 -0500

    -

--- SimeonInstaller.ps1
+++ SimeonInstaller.ps1
@@ -1149,10 +1149,10 @@ CRLFOption=CRLFAlways
                 value = $ExcludePipelinesFromEmail
             }
             SmtpServer = @{
-                SmtpServer = $SmtpServer
+                value = $SmtpServer
             }
             SmtpPort = @{
-                SmtpPort = $SmtpPort
+                value = "$SmtpPort"
             }
         }
         $queueName = $poolName = "Azure Pipelines"
