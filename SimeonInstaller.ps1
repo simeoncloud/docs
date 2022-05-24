3519d62f14a277dfe66d6b26822f61f3790dede8
Ricardo Dorta <rdorta@simeoncloud.com>
2022-05-24 20:22:02 -0300

    WIP

--- SimeonInstaller.ps1
+++ SimeonInstaller.ps1
@@ -1340,14 +1340,6 @@ CRLFOption=CRLFAlways
                 Initialize-GitConfiguration
                 Invoke-CommandLine "git remote add origin $($repo.remoteUrl) 2>&1" | Write-Verbose
 
-                $variablesContent = @"
-        {
-            "ResourceContext:TenantDomainName": "$Tenant"
-        }
-"@
-
-                $variablesContent | Set-Content variables.json
-
                 Invoke-CommandLine "git add . 2>&1" | Write-Verbose
                 Invoke-CommandLine "git commit -m 'Created Repository' 2>&1" | Write-Verbose
 
