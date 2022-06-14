9541c5a08a54b359ec66cade3ea75d7dbc36520a
Ricardo Dorta <rdorta@simeoncloud.com>
2022-06-14 20:29:12 -0300

    Forcing to push the tag even if it already exists

--- SimeonInstaller.ps1
+++ SimeonInstaller.ps1
@@ -1464,7 +1464,7 @@ CRLFOption=CRLFAlways
                 Write-Information "Pushing changes to remote repository"
                 Invoke-CommandLine 'git push origin master 2>&1' | Write-Verbose
                 if ($submodule) {
-                    Invoke-CommandLine 'git push origin master --tags 2>&1' | Write-Verbose
+                    Invoke-CommandLine 'git push origin master -f --tags 2>&1' | Write-Verbose
                 }
             }
         }
